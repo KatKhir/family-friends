79d1d60f040a5554c1088bb57a46b7f3c269d9a2
KatKhir <kasa0004@stud.kea.dk>
2026-04-13 09:09:45 +0200

    dog komponent næsten done

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,10 +2,10 @@ import { FiBell } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <nav className="flex justify-between mx-8 mb-10 mt-5">
+    <nav className="flex justify-between mx-6 mb-10 mt-5">
       <h1 className="text-black font-bold text-2xl">FamilyFriends</h1>
       <div className="relative">
-        <div className="absolute top-0.8 right-1 bg-red-300 rounded-full w-2.5 h-2.5 border-1 border-white"></div>
+        <div className="absolute top-0.8 right-1 bg-red-300 rounded-full w-2.5 h-2.5 border-1 border-gray-100"></div>
         <FiBell className="text-3xl" />
       </div>
     </nav>
--- src/app/components/Searchbar.jsx
+++ src/app/components/Searchbar.jsx
@@ -2,12 +2,12 @@ import { CgSearch } from "react-icons/cg";
 
 const SearchBar = () => {
   return (
-    <div className="flex gap-4 justify-center">
+    <div className="flex gap-4 mx-8 mb-10">
       <div className=" bg-red-300 rounded-full w-14 h-14 flex items-center justify-center">
         <CgSearch className="text-white text-3xl" />
       </div>
 
-      <input type="text" placeholder="search breeds" className="border border-gray-400 px-4 text-gray-500 p-2 rounded-full" />
+      <input type="text" placeholder="search breeds" className="border border-gray-400 px-4 text-gray-500 p-2 rounded-full w-100" />
     </div>
   );
 };
