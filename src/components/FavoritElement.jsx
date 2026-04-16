45fd539cedc78dedc0de5e3bb0c912943b173e4e
KatKhir <kasa0004@stud.kea.dk>
2026-04-16 22:52:24 +0200

    favorit dynamisk done

--- src/components/FavoritElement.jsx
+++ src/components/FavoritElement.jsx
@@ -1,11 +1,28 @@
+"use client";
 import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import useFavorites from "@/store/favorite";
 
-const FavoritElement = () => {
-  return (
-    <div className="absolute top-2 right-2 bg-white/35 rounded-full w-9 h-9 flex items-center justify-center ">
-      <FaRegStar className="text-white text-2xl" />
-    </div>
-  );
+const FavoritElement = ({ id, breed }) => {
+  const { favorites, setFavorites, removeFavorite } = useFavorites();
+  if (favorites.some((favorite) => favorite.id === id)) {
+    return (
+      <div className="absolute top-2 right-2 bg-white/35 rounded-full w-10 h-10 flex items-center justify-center ">
+        <FaStar className="text-yellow-500 text-3xl" onClick={() => removeFavorite(id)} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="absolute top-2 right-2 bg-white/35 rounded-full w-10 h-10 flex items-center justify-center ">
+        <FaRegStar
+          className="text-white text-3xl"
+          onClick={() => {
+            setFavorites(id, breed);
+          }}
+        />
+      </div>
+    );
+  }
 };
 
 export default FavoritElement;
---
+++ src/store/favorite.jsx
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+import { persist } from "zustand/middleware";
+
+const useFavorites = create(
+  persist((set, get) => ({
+    favorites: [],
+    setFavorites: (id, breed) =>
+      set((state) => ({
+        favorites: [...state.favorites, { id: id, breed: breed }],
+      })),
+    removeFavorite: (id) =>
+      set(() => ({
+        favorites: get().favorites.filter((favorite) => favorite.id !== id),
+      })),
+  })),
+);
+
+export default useFavorites;
